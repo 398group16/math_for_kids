576893be7d1a7e62a96b36d9353748c5ba28ad9d
shuo737 <foster336699@gmail.com>
2016-03-21 16:25:23 -0600

    re-push milestone 3

---
+++ MathForKids/MathForKids/ScoreListViewController.h
@@ -0,0 +1,28 @@
+//
+//  ScoreListViewController.h
+//  MathForKids
+//
+//  Created by Shuo Yuan on 2016-03-09.
+//  Copyright © 2016 398group16. All rights reserved.
+//
+#import "GraphView.h"
+#import <UIKit/UIKit.h>
+
+@interface ScoreListViewController : UIViewController{
+    IBOutlet GraphView* graph;
+
+    IBOutlet UIButton* count;
+    IBOutlet UIButton* add;
+    IBOutlet UIButton* sub;
+    IBOutlet UIButton* shape;
+}
+
+@property (weak, nonatomic) IBOutlet UIScrollView *scroller;
+@property (nonatomic, strong) NSDictionary* scoreDict;
+-(void)setScoreDict:(NSDictionary*) newDict;
+-(IBAction)countAction:(id)sender;
+-(IBAction)addAction:(id)sender;
+-(IBAction)subAction:(id)sender;
+-(IBAction)shapeAction:(id)sender;
+
+@end
